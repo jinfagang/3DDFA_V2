2d6be9cd3c268951f0277a6e31baf7e35b7cf23d
lewisjin <you@example.com>
2022-12-26 16:37:01 +0800

    fix mnn inference

--- export_onnx.py
+++ export_onnx.py
@@ -8,6 +8,7 @@ from utils.tddfa_util import _parse_param, load_model, str2bool
 import yaml
 from torch import nn
 from alfred import print_shape
+from TDDFA import TDDFA
 
 """
 Add export onnx model,
@@ -19,6 +20,10 @@ be note the keypoints are sparsed not dense, it's not through BFM model yet.
 """
 
 
+cfg_file = 'configs/mb1_120x120.yml'
+cfg = yaml.load(open(cfg_file), Loader=yaml.SafeLoader)
+test_img = 'data/emma.jpg'
+
 def reshape_fortran(x, shape):
     if len(x.shape) > 0:
         x = x.permute(*reversed(range(len(x.shape))))
@@ -28,36 +33,47 @@ def reshape_fortran(x, shape):
 class TDDFA_Pts2D(nn.Module):
     def __init__(self, **kvs) -> None:
         super().__init__()
-        size = kvs.get("size", 120)
+        # size = kvs.get("size", 120)
 
-        self.model = getattr(models, kvs.get("arch"))(
-            num_classes=kvs.get("num_params", 62),
-            widen_factor=kvs.get("widen_factor", 1),
-            size=size,
-            mode=kvs.get("mode", "small"),
-        )
-        checkpoint_fp = kvs.get("checkpoint_fp")
-        self.model = load_model(self.model, checkpoint_fp)
-        self.model.eval()
+        # self.model = getattr(models, kvs.get("arch"))(
+        #     num_classes=kvs.get("num_params", 62),
+        #     widen_factor=kvs.get("widen_factor", 1),
+        #     size=size,
+        #     mode=kvs.get("mode", "small"),
+        # )
+        # checkpoint_fp = kvs.get("checkpoint_fp")
+        # self.model = load_model(self.model, checkpoint_fp)
+        # self.model.eval()
 
-        bfm = BFMModel(
-            bfm_fp=kvs.get("bfm_fp", "configs/bfm_noneck_v3.pkl"),
-            shape_dim=kvs.get("shape_dim", 40),
-            exp_dim=kvs.get("exp_dim", 10),
-        )
-        # self.tri = bfm.tri
-        self.u_base, self.w_shp_base, self.w_exp_base = (
-            bfm.u_base,
-            bfm.w_shp_base,
-            bfm.w_exp_base,
-        )
-        self.u_base = torch.as_tensor(self.u_base)
-        self.w_shp_base = torch.as_tensor(self.w_shp_base)
-        self.w_exp_base = torch.as_tensor(self.w_exp_base)
+        # bfm = BFMModel(
+        #     bfm_fp=kvs.get("bfm_fp", "configs/bfm_noneck_v3.pkl"),
+        #     shape_dim=kvs.get("shape_dim", 40),
+        #     exp_dim=kvs.get("exp_dim", 10),
+        # )
+        # # self.tri = bfm.tri
+        # self.u_base, self.w_shp_base, self.w_exp_base = (
+        #     bfm.u_base,
+        #     bfm.w_shp_base,
+        #     bfm.w_exp_base,
+        # )
+        # self.u_base = torch.as_tensor(self.u_base)
+        # self.w_shp_base = torch.as_tensor(self.w_shp_base)
+        # self.w_exp_base = torch.as_tensor(self.w_exp_base)
 
-        r = pickle.load(open("configs/param_mean_std_62d_120x120.pkl", "rb"))
-        self.param_mean = torch.as_tensor(r.get("mean"))
-        self.param_std = torch.as_tensor(r.get("std"))  # 62,
+        # r = pickle.load(open("configs/param_mean_std_62d_120x120.pkl", "rb"))
+        # self.param_mean = torch.as_tensor(r.get("mean"))
+        # self.param_std = torch.as_tensor(r.get("std"))  # 62,
+
+        tddfa = TDDFA(**cfg)
+        self.model = tddfa.model
+        self.size = tddfa.size
+        self.transform = tddfa.transform
+        self.param_std = torch.from_numpy(tddfa.param_std)
+        self.param_mean = torch.from_numpy(tddfa.param_mean)
+        self.u_base = torch.from_numpy(tddfa.bfm.u_base)
+        self.w_shp_base = torch.from_numpy(tddfa.bfm.w_shp_base)
+        self.w_exp_base = torch.from_numpy(tddfa.bfm.w_exp_base)
+        self.eval()
 
     def forward(self, x):
         param = self.model(x)
@@ -66,23 +82,58 @@ class TDDFA_Pts2D(nn.Module):
 
         trans_dim, shape_dim, exp_dim = 12, 40, 10
         bs = param.shape[0]
+
+        R, offset, alpha_shp, alpha_exp = self._parse_param_batched(param)
+        # vertex = torch.transpose((self.u_base + self.w_shp_base @ alpha_shp + self.w_exp_base @ alpha_exp).reshape(bs, -1, 3), 0, 1)
+        vertex = torch.transpose((self.u_base + self.w_shp_base @ alpha_shp + self.w_exp_base @ alpha_exp).reshape(bs, -1, 3), 1, 2)
+        print_shape(R, offset, vertex)
+        # pts3d =  torch.transpose(R@vertex + offset, 0, 1)
+        pts3d =  torch.transpose(R@vertex + offset, 1, 2)
+        return param, pts3d
+    
+    @staticmethod
+    def _parse_param_batched(param):
+        n = param.shape[-1]
+        if n == 62:
+            trans_dim, shape_dim, exp_dim = 12, 40, 10
+        elif n == 72:
+            trans_dim, shape_dim, exp_dim = 12, 40, 20
+        elif n == 141:
+            trans_dim, shape_dim, exp_dim = 12, 100, 29
+        else:
+            raise Exception(f'Undefined templated param parsing rule')
+
+        bs = param.shape[0]
         R_ = param[:, :trans_dim].reshape(bs, 3, -1)
-        print_shape(R_)
         R = R_[:, :, :3]
         offset = R_[:, :, -1].reshape(bs, 3, 1)
-        alpha_shp = param[:, trans_dim : trans_dim + shape_dim].reshape(bs, -1, 1)
-        alpha_exp = param[:, trans_dim + shape_dim :].reshape(bs, -1, 1)
+        alpha_shp = param[..., trans_dim:trans_dim + shape_dim].reshape(bs, -1, 1)
+        alpha_exp = param[..., trans_dim + shape_dim:].reshape(bs, -1, 1)
+        return R, offset, alpha_shp, alpha_exp
+    
+    @staticmethod
+    def _parse_param(param):
+        """matrix pose form
+        param: shape=(trans_dim+shape_dim+exp_dim,), i.e., 62 = 12 + 40 + 10
+        """
 
-        print_shape(self.u_base, self.w_shp_base, self.w_exp_base)
-        print_shape(alpha_shp, alpha_exp)
-        # 204,1 + 204,40 @ 1,40,1  + 204,10 @ 1,10,1
-        pts3d = self.u_base + self.w_shp_base @ alpha_shp + self.w_exp_base @ alpha_exp
-        pts3d = reshape_fortran(pts3d, [bs, 3, -1])
-        pts3d = R @ pts3d
-        pts3d = pts3d + offset
-        print_shape(pts3d)
-        return param, pts3d
+        # pre-defined templates for parameter
+        n = param.shape[-1]
+        if n == 62:
+            trans_dim, shape_dim, exp_dim = 12, 40, 10
+        elif n == 72:
+            trans_dim, shape_dim, exp_dim = 12, 40, 20
+        elif n == 141:
+            trans_dim, shape_dim, exp_dim = 12, 100, 29
+        else:
+            raise Exception(f'Undefined templated param parsing rule')
 
+        R_ = param[..., :trans_dim].reshape(3, -1)
+        R = R_[:, :3]
+        offset = R_[:, -1].reshape(3, 1)
+        alpha_shp = param[..., trans_dim:trans_dim + shape_dim].reshape(-1, 1)
+        alpha_exp = param[..., trans_dim + shape_dim:].reshape(-1, 1)
+        return R, offset, alpha_shp, alpha_exp
 
 def convert_to_onnx(**kvs):
     # 1. load model
@@ -101,9 +152,10 @@ def convert_to_onnx(**kvs):
         (dummy_input,),
         wfp,
         input_names=["input"],
-        output_names=["param", "pts3d"],
+        # output_names=["pts3dparam"],
+        output_names=["param", "output"],
+        opset_version=9,
         do_constant_folding=True,
-        opset_version=13
     )
     print(f"Convert {checkpoint_fp} to {wfp} done.")
     traced_model = torch.jit.trace(wrapmodel, (dummy_input,))
